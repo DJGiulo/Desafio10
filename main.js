c71bfff0a7385ebd0985c8a9f185f50504e329aa
Giulo <giulianolezcano57@gmail.com>
2024-12-16 02:47:05 -0300

    5

---
+++ main.js
@@ -0,0 +1,6 @@
+const tiradaGeneral = 6000
+const tirada_si_o_no = 800
+const tirada3cartas = 1999 //promo
+const tiradaAmor = 2600
+const tiradaArcanoMayor = 4000
+//Preguntas de salud fisica no se responden
